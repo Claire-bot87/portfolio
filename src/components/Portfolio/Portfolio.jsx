9af82ed0725a1080260beb9bd8cc900cbfdfbb23
Claire <clairebrady12@hotmail.com>
2025-03-27 13:55:15 +0000

    changed a type

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import { Carousel } from '../../components/Carousel/Carousel.jsx'
 import { slides } from '../../data/carouselData.json'
-import './portfolio.css'
+import './Portfolio.css'
 
 export default function Portfolio(){
     return (
