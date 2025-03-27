0c014755f66c0859cfef8b88e5e6c5fd3a5ea0e1
Claire <clairebrady12@hotmail.com>
2025-03-27 13:41:03 +0000

    added links

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
 import './Carousel.css'
+import { Link } from 'react-router'
 
 
 
@@ -23,14 +24,19 @@ export const Carousel = ({ data }) => {
             
             {data.map((item, idx) => (
     
-            
+    
                  <div   
                  key={idx}
                  className={slide === idx ? 'div-wrapper' : 'div-wrapper div-hidden'}
                  >
                      <div className={slide === idx ? 'div-left' : 'div-left-hidden'}>
+                     <a href={item.project_link} target="_blank" rel="noopener noreferrer">
                         <h1>{item.title}</h1>
+                        </a>
+                        <a href={item.readme_link} target="_blank" rel="noopener noreferrer">
                         <p>{item.text}</p>
+                
+                        </a>
                         </div>
                 <div  className={slide === idx ? 'div-right' : 'div-right-hidden'}>
                     <img src={item.src}
@@ -38,9 +44,7 @@ export const Carousel = ({ data }) => {
                     className='slide' />
                     </div>
                    
-                        </div>
-                    
-                   
+                        </div> 
            ) )}
            
             < BsArrowRightCircleFill className='arrow arrow-right' onClick={nextSlide} />
